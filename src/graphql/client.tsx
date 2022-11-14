a2a5fdbc0aa7375481e594467584904a723ceffd
Ilari Perämäki <n8peil00@students.oamk.fi>
2022-11-14 14:17:13 +0200

    Add client config and provider component

---
+++ src/graphql/client.tsx
@@ -0,0 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import Config from '../../apollo.config.js';
+
+/*This could also just be actual url since there were issues with
+* automated tools and the target schema
+*/
+const API_URI = Config.client.service.url;
+
+const apolloCache = new InMemoryCache();
+
+const graphQLClient = new ApolloClient({
+    cache : apolloCache,
+    uri : API_URI
+});
+
+
+export type GraphQLProviderProps =
+{
+    children: React.ReactNode | React.ReactNode[] | null;
+}
+
+export function GraphQLProvider({children} : GraphQLProviderProps)  {
+    return(<ApolloProvider client={graphQLClient}>...children</ApolloProvider>);
+}
