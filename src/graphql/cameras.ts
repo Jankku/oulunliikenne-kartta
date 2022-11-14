fca50401dadd5f0e2c88b7c5df07c5ed2847e9ff
Ilari Perämäki <n8peil00@students.oamk.fi>
2022-11-14 14:25:33 +0200

    Initial query setup for cameras

---
+++ src/graphql/cameras.ts
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client';
+import { minutesToMs } from '../utils/time';
+
+/**
+ * Actual frequency is 5-15 minutes so use 5 minutes
+ */
+const POLL_FREQUENCY = minutesToMs(5);
+
+const QUERY = gql`
+  query GetAllCameras {
+    cameras {
+      cameraId
+      name
+      lat
+      lon
+      presets {
+        presetId
+        presentationName
+        imageUrl
+        measuredTime
+      }
+    }
+  }
+`;
