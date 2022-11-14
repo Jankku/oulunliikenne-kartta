0b2a87c709da208dbaad94377dec6652793c4f58
Ilari Perämäki <n8peil00@students.oamk.fi>
2022-11-14 14:24:17 +0200

    Fix formatting for graphql client

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -1,25 +1,23 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
 import Config from '../../apollo.config.js';
 
 /*This could also just be actual url since there were issues with
-* automated tools and the target schema
-*/
+ * automated tools and the target schema
+ */
 const API_URI = Config.client.service.url;
 
 const apolloCache = new InMemoryCache();
 
 const graphQLClient = new ApolloClient({
-    cache : apolloCache,
-    uri : API_URI
+  cache: apolloCache,
+  uri: API_URI,
 });
 
+export type GraphQLProviderProps = {
+  children: React.ReactNode | React.ReactNode[] | null;
+};
 
-export type GraphQLProviderProps =
-{
-    children: React.ReactNode | React.ReactNode[] | null;
-}
-
-export function GraphQLProvider({children} : GraphQLProviderProps)  {
-    return(<ApolloProvider client={graphQLClient}>...children</ApolloProvider>);
+export function GraphQLProvider({ children }: GraphQLProviderProps) {
+  return <ApolloProvider client={graphQLClient}>...children</ApolloProvider>;
 }
