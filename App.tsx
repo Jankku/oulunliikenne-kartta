13e297d3f374c64b5439d9035cb79ebeee0a4814
Samppa Kivelä <n0kisa00@students.oamk.fi>
2022-11-16 16:01:13 +0200

    Bottom Navigation

--- App.tsx
+++ App.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import { BottomNav } from './src/navigation/nav';
 import { GraphQLProvider } from './src/graphql/client';
-import { StyleSheet, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <GraphQLProvider>
-      <View style={styles.container}>
-        <MapView style={styles.map} />
-      </View>
+        <BottomNav/>
     </GraphQLProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+
