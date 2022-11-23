3cdad828b5cc78cb2925bfddbf960022b49c2a3d
Ilari Perämäki <n8peil00@students.oamk.fi>
2022-11-23 14:31:57 +0200

    Refactor hook result typestate to a generic

--- src/hooks/map/useTrafficCameras.tsx
+++ src/hooks/map/useTrafficCameras.tsx
@@ -1,25 +1,9 @@
-import { QueryError, toOpaqueError as toOpaqueError } from '../../graphql/error';
+import { toOpaqueError } from '../../graphql/error';
 import { SchemaType, QueryResult, useQuery } from '../../graphql/cameras';
 import { CameraModel, fromSchemaToModel } from '../../models/camera';
+import { QueryHookResult } from '../../graphql/result';
 
-//This might be a bad assumption, but let's just show the freshest data or nothing
-export type LoadingState = {
-  loading: true;
-};
-
-export type ErrorState = {
-  loading: false;
-  error: QueryError;
-};
-
-//Current error settings in client dictates either data or error. Not both
-export type ResultState = {
-  loading: false;
-  error?: undefined;
-  data: CameraModel[];
-};
-
-export type TrafficCameraData = ResultState | ErrorState | LoadingState;
+export type TrafficCameraData = QueryHookResult<CameraModel[]>;
 
 export default function useTrafficCameras(): TrafficCameraData {
   const { loading, data, error }: QueryResult = useQuery();
