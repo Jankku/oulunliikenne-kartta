44a182345bf7a36b66e73a6794fa02a29e9cf0a0
Jankku <29043938+Jankku@users.noreply.github.com>
2022-12-09 15:48:53 +0200

    About page changes
    Open GitHub link in browser
    Change button design

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,22 +1,72 @@
-import { Image, View, Text, Pressable } from 'react-native';
+import { Image, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import styles from '../styles/styles';
-import { Title } from 'react-native-paper';
+import { Button, Title, Text } from 'react-native-paper';
+import * as WebBrowser from 'expo-web-browser';
 
 export default function About() {
+  const openGitHub = async () => {
+    try {
+      await WebBrowser.openBrowserAsync('https://github.com/Jankku/oulunliikenne-kartta');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.about}>
       <Image style={styles.image} source={{ uri: 'https://placekitten.com/g/400/250' }} />
-      <Title style={styles.title}>Kelikamera</Title>
+      <Title style={styles.title}>Oulun Liikennekartta</Title>
       <Text style={styles.text}>Lyhyt kuvaus teksti sovelluksesta</Text>
-      <View style={styles.row}>
-        <Pressable style={styles.pressable}>
-          <Text style={styles.pressableText}>Github</Text>
-        </Pressable>
-        <Pressable style={styles.pressable}>
-          <Text style={styles.pressableText}>Lisenssi</Text>
-        </Pressable>
+      <View style={styles.buttonContainer}>
+        <Button
+          mode="contained"
+          style={styles.button}
+          labelStyle={styles.buttonText}
+          onPress={openGitHub}
+        >
+          GitHub
+        </Button>
+        <Button mode="contained" style={styles.button} labelStyle={styles.buttonText}>
+          Lisenssi
+        </Button>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  about: {
+    alignItems: 'center',
+    flex: 1,
+    textAlign: 'center',
+  },
+  button: {
+    borderRadius: 8,
+    fontSize: '24px',
+    height: 50,
+    margin: 30,
+    padding: 6,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  buttonText: {
+    fontSize: 18,
+  },
+  image: {
+    borderRadius: 100,
+    height: 200,
+    marginBottom: 50,
+    marginTop: 100,
+    resizeMode: 'cover',
+    width: 200,
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 50,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+  },
+});
