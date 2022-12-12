c204acd958ecdbdf357fca8cc68e26ecfacaabe9
Jankku <29043938+Jankku@users.noreply.github.com>
2022-12-12 12:30:52 +0200

    Fix AppBar back button placement

--- src/navigation/CustomAppBar.tsx
+++ src/navigation/CustomAppBar.tsx
@@ -4,8 +4,8 @@ import { StackHeaderProps } from '@react-navigation/stack';
 export default function CustomAppBar({ navigation, options, route, back }: StackHeaderProps) {
   return (
     <Appbar.Header>
-      <Appbar.Content title={options.title ?? route.name} />
       {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title={options.title ?? route.name} />
       <>{options.headerRight ? options.headerRight : null}</>
     </Appbar.Header>
   );
